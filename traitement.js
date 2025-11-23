17a604164cfb699ab98ef10d2eb0fc17d6c31071
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2025-11-23 10:55:26 -0500

    Add JSON file read/write utility functions
    Introduced readData and writeData functions to handle reading from and writing to the commandes.json file. These utilities will help manage persistent data storage for the application.

--- traitement.js
+++ traitement.js
@@ -3,6 +3,17 @@ import fs from "node:fs";
 
 const PORT = 8001;
 const HOSTNAME = "127.0.0.1";
+const FILE = "./commandes.json";
+
+// lire le fichiers json
+function readData() {
+  return JSON.parse(fs.readFileSync(FILE, "utf-8"));
+}
+
+// Écrire dans le fichier JSON
+function writeData(data) {
+  fs.writeFileSync(FILE, JSON.stringify(data, null, 2));
+}
 
 // Création du serveur HTTP
 const server = http.createServer((req, res) => {
