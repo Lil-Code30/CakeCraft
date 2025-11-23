5f038e1333174cd1ccc1a2c182adc3b22967941e
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2025-11-22 23:12:41 -0500

    Add flavor selection and update cake image logic
    Updated the flavor select options in commander.html to use new values. In commander.js, added event handling for flavor selection, updating the cake image based on both base and flavor, and providing user feedback for invalid selections.

--- js/commander.js
+++ js/commander.js
@@ -1,5 +1,6 @@
 const baseCake = document.getElementById("base-cake");
 const cakeContainer = document.getElementById("cake-container");
+const cakeFavor = document.getElementById("cake-flavor");
 
 const cakeImage = document.createElement("img");
 cakeImage.id = "cake-image";
@@ -10,13 +11,59 @@ baseCake.addEventListener("change", (event) => {
 
   let baseImageSrc = "";
   switch (selectedBase) {
-    case "vanille":
+    case "base_vanille":
       baseImageSrc = "./assets/images/base_vanille.png";
       break;
-    case "chocolat":
+    case "base_chocolat":
       baseImageSrc = "./assets/images/base_chocolat.png";
       break;
   }
   cakeImage.src = baseImageSrc;
   cakeImage.alt = `Base de gâteau ${selectedBase}`;
+});
+
+cakeFavor.addEventListener("change", (event) => {
+  const selectedFlavor = event.target.value;
+
+  let flavorImageSrc = "";
+  const selectedBase = baseCake.value;
+  if (cakeImage.src) {
+    switch (selectedFlavor) {
+      case "vanille":
+        if (selectedBase === "base_vanille") {
+          flavorImageSrc = "./assets/images/base_vanille_glacage_vanille.png";
+        } else if (selectedBase === "base_chocolat") {
+          flavorImageSrc = "./assets/images/base_chocolat_glacage_vanille.png";
+        }
+        break;
+      case "chocolat":
+        if (selectedBase === "base_vanille") {
+          flavorImageSrc = "./assets/images/base_vanille_glacage_chocolat.png";
+        } else if (selectedBase === "base_chocolat") {
+          flavorImageSrc = "./assets/images/base_chocolat_glacage_chocolat.png";
+        }
+        break;
+      case "fraise":
+        if (selectedBase === "base_vanille") {
+          flavorImageSrc = "./assets/images/base_vanille_glacage_fraise.png";
+        } else if (selectedBase === "base_chocolat") {
+          flavorImageSrc = "./assets/images/base_chocolat_glacage_fraise.png";
+        }
+        break;
+    }
+    if (flavorImageSrc) {
+      cakeImage.src = flavorImageSrc;
+      cakeImage.alt = `Gâteau avec base ${selectedBase} et glaçage ${selectedFlavor}`;
+    } else {
+      alert(
+        "La saveur sélectionnée n'est pas disponible pour la base choisie."
+      );
+      cakeFavor.value = "";
+      return;
+    }
+  } else {
+    alert("Veuillez d'abord sélectionner une base de gâteau.");
+    cakeFavor.value = "";
+    return;
+  }
 });
